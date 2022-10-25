6eefd3abff17acd60cf85720010ad8fb6ec7dfe9
FrontDev <57919474+Meteo-Flagyl00@users.noreply.github.com>
2022-10-25 14:32:09 +0100

    showing the Skills in our page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import Skills from '../components/Skills'
 import WorkExperience from '../components/WorkExperience'
 
 
@@ -29,6 +30,10 @@ const Home: NextPage = () => {
       </section>
 
       {/* skill */}
+      <section id="skills" className='snap-center'>
+        <Skills/>
+      </section>
+      
       {/* Projects */}
       {/* Contact Me */}
     </div>
